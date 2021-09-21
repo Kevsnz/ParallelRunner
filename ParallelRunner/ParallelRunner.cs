c9ff2b65b116106f5773ab34f627b4a3b7ccbc92
Kevsnz <15689588+Kevsnz@users.noreply.github.com>
2021-09-21 18:38:05 +0500

    First commit

---
+++ ParallelRunner/ParallelRunner.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelRunnerNs
+{
+	public static class ParallelRunner
+	{
+		private class TaskData
+		{
+			public Action<int> Task { get; private set; }
+			public int Index;
+			public int Count { get; private set; }
+
+			public TaskData ( Action<int> task, int count )
+			{
+				Task = task;
+				Count = count;
+				Index = 0;
+			}
+		}
+
+		private static void threadTask ( object d )
+		{
+			var td = ( TaskData ) d;
+			while ( true )
+			{
+				int idx = Interlocked.Add ( ref td.Index, 1 ) - 1;
+
+				if ( idx >= td.Count )
+					return;
+				td.Task ( idx );
+			}
+		}
+
+		private static readonly int _processorCount = Environment.ProcessorCount;
+		public static void RunInParallel ( Action<int> pt, int elementCount ) { RunInParallel ( pt, elementCount, _processorCount ); }
+		public static void RunInParallel ( Action<int> pt, int elementCount, int threadCount )
+		{
+			if ( elementCount == 0 )
+				return;
+
+			if ( elementCount == 1 )
+			{
+				pt ( 0 );
+				return;
+			}
+
+			if ( threadCount < 2 )
+				throw new ArgumentException ( "Not enough threads to run something in parallel", "threadCount" );
+
+			var td = new TaskData ( pt, elementCount );
+
+			var tasks = new Task[ Math.Min ( elementCount, threadCount )-1 ];
+			for ( int i = 0; i < tasks.Length; i++ )
+			{
+				tasks[ i ] = new Task ( threadTask, td );
+				tasks[ i ].Start ();
+			}
+
+			threadTask ( td );
+			//Task.WaitAll ( tasks, -1 );
+			for ( int i = 0; i < tasks.Length; i++ )
+				tasks[ i ].Wait ();
+		}
+
+		public static void SyncDo ( ref SpinLock sl, Action action )
+		{
+			bool gotLock = false;
+			try
+			{
+				sl.Enter ( ref gotLock );
+				action ();
+			}
+			finally
+			{
+				if ( gotLock )
+					sl.Exit ();
+			}
+		}
+	}
+}
---
+++ TestProj/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+using ParallelRunnerNs;
+
+namespace TestProj
+{
+	internal class Program
+	{
+		internal static void Main ( string[] args )
+		{
+			Console.WriteLine ( "Welcome!" );
+
+			var timer = new Stopwatch ();
+			var rnd = new Random ( 1 );
+
+			var data = new double[ 5000 ];
+			for ( int i = 0; i < data.Length; i++ )
+				data[ i ] = rnd.NextDouble () * 6 - 3;
+
+			var res = new double[ data.Length ];
+
+			timer.Start ();
+
+			/*for ( int i = 0; i < data.Length; i++ ) // ~ 125k ticks
+				res[ i ] = func ( data[ i ] ); //*/
+
+			//ParallelRunner.RunInParallel ( i => res[ i ] = func ( data[ i ] ), data.Length ); // ~90k ticks
+
+
+			/*for ( int k = 0; k < 10000; k++ ) // ~11.9M ticks
+				for ( int i = 0; i < data.Length; i++ )
+					res[ i ] = func ( data[ i ] ); //*/
+
+			/*for ( int k = 0; k < 10000; k++ ) // ~5.0M ticks
+				ParallelRunner.RunInParallel ( i => res[ i ] = func ( data[ i ] ), data.Length ); //*/
+
+			ParallelRunner.RunInParallel ( i => res[ i / 10000 ] = func ( data[ i / 10000 ] ), data.Length * 10000 ); // ~4.5M ticks ??????????????????????? */
+
+			timer.Stop ();
+
+			Console.WriteLine ( "Result: " + res.Sum () );
+			Console.WriteLine ( "Time elapsed: " + ( timer.ElapsedTicks * 1000.0 / Stopwatch.Frequency ).ToString ( "0.000000" ) + " ms (" + timer.ElapsedTicks + " ticks)." );
+
+			Console.WriteLine ( "Done!" );
+			Console.ReadKey ( true );
+		}
+
+		private static double func ( double val ) { return Math.Sqrt ( Math.Cos ( Math.Tanh ( val ) ) ); }
+	}
+}
